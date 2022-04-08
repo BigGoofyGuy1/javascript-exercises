67a6cb1522f754483666014723ea82f016afaf3c
BigGoofyGuy <collin.weatherford@gmail.com>
2022-04-08 13:56:51 -0500

    Completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...num) {
+    
+    for (let i =0; i < array.length; i++) {
+        console.log("array:" + array[i])
+        
+        for( let j of [...num]){ 
+            console.log("num: " + j)
 
+            if (array[i] === j) {
+                array.splice(i, 1)
+                i--
+                
+            }            
+        }
+        
+    }
+
+    return array
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
